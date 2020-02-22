ad8ffa2eed9487c8cebecd9abf3f9dc4b93dd7e7
Charles Pierse <charlespierse@gmail.com>
2020-02-22 00:52:56 +0000

    Added training loop

---
+++ data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+RED_WINE_PATH = "data/winedata/winequality_red.csv"
+WHITE_WINE_PATH = "data/winedata/winequality_white.csv"
+def read_data(path):
+    return pd.read_csv(path)
+
+def get_model_data(data : pd.DataFrame()):
+    if "quality" not in data.columns:
+        raise Exception("Sorry quality feature is not in this dataset") 
+    else:
+        target = data['quality']
+        inputs = data.drop(columns=['quality'])
+        return(inputs,pd.get_dummies(target))
+def split_data(x,y):
+    return train_test_split(x,y,test_size=0.2, random_state =42)
+
+
+if __name__ == "__main__":
+    data = read_data(RED_WINE_PATH)
+    x,y = get_model_data(data)
+    print(split_data(x,y)) 
+
--- network.py
+++ network.py
@@ -1,11 +1,76 @@
 import torch
 import torch.nn as nn
+from data import get_model_data, read_data, split_data, RED_WINE_PATH, WHITE_WINE_PATH
 
 
 
 class NeuralNet(nn.Module):
-    def __init__(self):
+    def __init__(self,input_size, hidden_size,number_of_classes):
         super().__init__()
+        self.input_size = input_size
+        self.hidden_size = input_size
+        self.number_of_classes = number_of_classes
+        print(number_of_classes)
+
+        # Hidden layer
+        self.fc1 = nn.Linear(self.input_size,self.hidden_size)
+        self.relu = nn.ReLU()
+        # Output layer
+        self.fc2 = nn.Linear(self.hidden_size, self.number_of_classes)
+        self.softmax = nn.Softmax()
+
+
+    def forward(self,x):
+        hidden = self.fc1(x)
+        relu = self.relu(hidden)
+        output = self.fc2(relu)
+        output = self.softmax(output)
+        return output
+
         
+
+
+if __name__ == "__main__":
+    data = read_data(RED_WINE_PATH)
+    x,y = get_model_data(data)
+    x_train,x_test,y_train, y_test = split_data(x.values,y.values)
+    input_size = x_train.shape[1]
+    hidden_size = 10
+    no_of_classes = 6
+    x_train,x_test,y_train,y_test = torch.FloatTensor(x_train),torch.FloatTensor(x_test), torch.as_tensor(y_train),torch.as_tensor(y_test)
+    y_train, y_test = torch.argmax(y_train, dim=1), torch.argmax(y_test, dim=1)
+
+
+    
+    model = NeuralNet(input_size,hidden_size,no_of_classes)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(),lr = 0.01)
+
+
+    model.eval()
+    y_pred = model(x_test)
+    before_train = criterion(y_pred.squeeze(), y_test)
+    print('Test loss before training' , before_train.item())
+
+    model.train()
+    epoch = 1000
+    print(y_train.shape)
+
+    for epoch in range(epoch):
+
+        optimizer.zero_grad()
+
+        y_pred = model(x_train)
+        loss = criterion(y_pred.squeeze(),y_train)
+        
+        print('Epoch {}: train loss: {}'.format(epoch, loss.item()))    # Backward pass
+        loss.backward()
+        optimizer.step()
+    i = 200
+    print(model(x_test[i]))
+    print(y_test[i])
+
+    
 
 
