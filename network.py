6c73088b9a9c50a15a8782392e2966ae02b33c59
Pierse <B756@europa.internal>
2020-02-24 17:46:30 +0000

    Add outline for training loop code

--- network.py
+++ network.py
@@ -3,9 +3,8 @@ import torch.nn as nn
 from data import get_model_data, read_data, split_data, RED_WINE_PATH, WHITE_WINE_PATH
 
 
-
 class NeuralNet(nn.Module):
-    def __init__(self,input_size, hidden_size,number_of_classes):
+    def __init__(self, input_size, hidden_size, number_of_classes):
         super().__init__()
         self.input_size = input_size
         self.hidden_size = input_size
@@ -13,48 +12,76 @@ class NeuralNet(nn.Module):
         print(number_of_classes)
 
         # Hidden layer
-        self.fc1 = nn.Linear(self.input_size,self.hidden_size)
+        self.fc1 = nn.Linear(self.input_size, self.hidden_size)
         self.relu = nn.ReLU()
         # Output layer
         self.fc2 = nn.Linear(self.hidden_size, self.number_of_classes)
         self.softmax = nn.Softmax()
 
-
-    def forward(self,x):
+    def forward(self, x):
         hidden = self.fc1(x)
         relu = self.relu(hidden)
         output = self.fc2(relu)
         output = self.softmax(output)
         return output
 
-        
 
-
-if __name__ == "__main__":
+def train():
     data = read_data(RED_WINE_PATH)
-    x,y = get_model_data(data)
-    x_train,x_test,y_train, y_test = split_data(x.values,y.values)
+    x, y = get_model_data(data)
+    x_train, x_test, y_train, y_test = split_data(x.values, y.values)
     input_size = x_train.shape[1]
     hidden_size = 10
     no_of_classes = 6
-    x_train,x_test,y_train,y_test = torch.FloatTensor(x_train),torch.FloatTensor(x_test), torch.as_tensor(y_train),torch.as_tensor(y_test)
+    x_train, x_test, y_train, y_test = (
+        torch.FloatTensor(x_train),
+        torch.FloatTensor(x_test),
+        torch.as_tensor(y_train),
+        torch.as_tensor(y_test),
+    )
     y_train, y_test = torch.argmax(y_train, dim=1), torch.argmax(y_test, dim=1)
 
-
-    
-    model = NeuralNet(input_size,hidden_size,no_of_classes)
+    model = NeuralNet(input_size, hidden_size, no_of_classes)
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(),lr = 0.01)
-
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
     model.eval()
     y_pred = model(x_test)
     before_train = criterion(y_pred.squeeze(), y_test)
-    print('Test loss before training' , before_train.item())
+    print("Test loss before training", before_train.item())
 
     model.train()
     epoch = 1000
+
+
+if __name__ == "__main__":
+    data = read_data(RED_WINE_PATH)
+    x, y = get_model_data(data)
+    x_train, x_test, y_train, y_test = split_data(x.values, y.values)
+    input_size = x_train.shape[1]
+    hidden_size = 10
+    no_of_classes = 6
+    x_train, x_test, y_train, y_test = (
+        torch.FloatTensor(x_train),
+        torch.FloatTensor(x_test),
+        torch.as_tensor(y_train),
+        torch.as_tensor(y_test),
+    )
+    y_train, y_test = torch.argmax(y_train, dim=1), torch.argmax(y_test, dim=1)
+
+    model = NeuralNet(input_size, hidden_size, no_of_classes)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    model.eval()
+    y_pred = model(x_test)
+    before_train = criterion(y_pred.squeeze(), y_test)
+    print("Test loss before training", before_train.item())
+
+    model.train()
+    epoch = 10000
     print(y_train.shape)
 
     for epoch in range(epoch):
@@ -62,15 +89,13 @@ if __name__ == "__main__":
         optimizer.zero_grad()
 
         y_pred = model(x_train)
-        loss = criterion(y_pred.squeeze(),y_train)
-        
-        print('Epoch {}: train loss: {}'.format(epoch, loss.item()))    # Backward pass
+        loss = criterion(y_pred.squeeze(), y_train)
+
+        print("Epoch {}: train loss: {}".format(epoch, loss.item()))  # Backward pass
         loss.backward()
         optimizer.step()
-    i = 200
+    i = 2
     print(model(x_test[i]))
+    print(torch.sigmoid(model(x_test[i])))
     print(y_test[i])
-
-    
-
 
